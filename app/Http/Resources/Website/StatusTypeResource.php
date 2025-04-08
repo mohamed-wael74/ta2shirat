8b51fd748aa6a00b14ca34954bf8d7b4113b7938
Mohamed Wael <mohamedwaelol74@gmail.com>
2025-04-08 16:27:20 +0200

    make api resource for the Status and Status Type and simple one for the User

---
+++ app/Http/Resources/Website/StatusTypeResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Website;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StatusTypeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type' => $this->type,
+            'color' => $this->color,
+            'main' => $this->is_main,
+        ];
+    }
+}
